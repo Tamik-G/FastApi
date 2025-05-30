96caa98d4f56f924c4ebcabc764342a897b645a7
Tamik-G <timurchaershin@mail.ru>
2025-05-30 19:35:37 +0300

    Создали фундамент. Подключили БД. Начали работать с миграциями. Определились с архитектурой API.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
 app = FastAPI()
 
-@app.get("/hotels/{hotel_id}")
-def get_hotels(hotel_id: int, date_from, date_to):
-    return hotel_id, date_to, date_from
+
+class HotelsSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: Optional[bool] = None,
+            stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+# эндпоинт для поиска отелей
+@app.get("/hotels")
+def get_hotels(
+    search_args: HotelsSearchArgs = Depends()
+):
+    return search_args
+
+# здесь указываем, какие поля хотим передать в пост запросе
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
